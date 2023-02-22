3446c06b8701a7d7b9fa6c32691055ce1a7e691d
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-02-22 16:54:41 +0200

    added fonts, made registration screen, started login screen

--- App.js
+++ App.js
@@ -1,83 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity,Platform} from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+
+import RegistrationScreen from './screens/auth/RegistrationScree';
+import LoginForm from './screens/auth/LoginScreen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  console.log(Platform.OS);
+  const [fontsLoaded] = useFonts({
+    Roboto_Regular: require('./assets/fonts/Roboto-Regular.ttf'),
+    Roboto_Medium: require('./assets/fonts/Roboto-Medium.ttf'),
+    Roboto_Bold: require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={require('./assets/PhotoBG.png')}>
-        <Text style={styles.title}>Реєстрація</Text>
-         <View style={styles.form} >
-          <TextInput placeholder="Логін" style={styles.input} />
-          <TextInput placeholder="Адрес електронної пошти" style={styles.input} />
-          <TextInput placeholder="Пароль" style={styles.input} secureTextEntry={true} />
-          <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
-            <Text style={styles.btnText}>Зареєструватися</Text>
-          </TouchableOpacity>
-          <Text style={styles.textAlready}>Вже є аккаунт? Увійти</Text>
-        </View>
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onLayout={onLayoutRootView}>
+      <RegistrationScreen />
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    // alignItems: 'center',
-  },
- 
-
-  input: {
-    borderWidth: 1,
-    backgroundColor: '#F6F6F6',
-    borderColor: '#E8E8E8',
-    borderStyle: 'solid',
-    height: 50,
-    borderRadius: 8,
-    // minWidth: 343,
-    color: '#212121',
-    paddingLeft: 16,
-    marginTop: 16,
-  },
-  title: {
-    color: '#212121',
-    fontSize: 30,
-    lineHeight: 35,
-    letterSpacing: "0.01em",
-    marginBottom: 16,
-    justifyContent: 'center',
-    alignItems:'center',
-  },
-    
-  form: {
-    marginHorizontal: 16,
-  },
-  btn: {
-    backgroundColor: '#FF6C00',
-    height: 51,
-    borderRadius:100,
-    marginTop: 43,
-    justifyContent: 'center',
-    alignItems:'center',
-  },
-  btnText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    lineHeight:19,
-  },
-  textAlready: {
-    marginTop:16,
-  }
-});
---
+++ screens/auth/RegistrationScree.js
@@ -0,0 +1,187 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from 'react-native';
+
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
+
+const initialState = {
+  login: '',
+  email: '',
+  password: '',
+};
+
+export default function RegistrationScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  // const [dimentions, setDimentions] = useState(Dimensions.get('window').width - 16 * 2);
+
+  // useEffect(() => {
+  //   const onChange = () => {
+  //     const windowWidth = Dimensions.get('window').width;
+  //     console.log(windowWidth);
+  //     setDimentions(windowWidth);
+  //   };
+  //   dimensionsHandler = Dimensions.addEventListener('change', onChange);
+  //   return () => {
+  //     () => dimensionsHandler.remove();
+  //   };
+  // }, []);
+
+  const onFocus = () => setIsShowKeyboard(true);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const submitData = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground style={styles.image} source={require('../../assets/PhotoBG.png')}>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <View style={styles.bottomForm}>
+              <View
+                style={{
+                  ...styles.registrationForm,
+                  marginBottom: isShowKeyboard ? -97 : 78,
+                }}
+              >
+                <Text style={styles.title}>Registration</Text>
+                <View style={styles.inputForm}>
+                  <TextInput
+                    placeholder="Login"
+                    style={styles.input}
+                    value={state.login}
+                    onFocus={onFocus}
+                    onChangeText={value => setState(presState => ({ ...presState, login: value }))}
+                    onSubmitEditing={keyboardHide}
+                  />
+                  <TextInput
+                    placeholder="Email"
+                    style={styles.input}
+                    value={state.email}
+                    onFocus={onFocus}
+                    onChangeText={value => setState(presState => ({ ...presState, email: value }))}
+                    onSubmitEditing={keyboardHide}
+                  />
+                  <TextInput
+                    placeholder="Password"
+                    style={styles.input}
+                    secureTextEntry={true}
+                    value={state.password}
+                    onFocus={onFocus}
+                    onChangeText={value =>
+                      setState(presState => ({ ...presState, password: value }))
+                    }
+                    onSubmitEditing={keyboardHide}
+                  />
+                  <TouchableOpacity style={styles.btn} activeOpacity={0.8} onPress={submitData}>
+                    <Text style={styles.btnText}>Register</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.bottomtext}>Already have an account? Sign in</Text>
+                </View>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+        {/* <StatusBar style="auto" /> */}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+    // alignItems: 'center',
+  },
+  bottomForm: {
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  registrationForm: {},
+  input: {
+    borderWidth: 1,
+    backgroundColor: '#F6F6F6',
+    borderColor: '#E8E8E8',
+    borderStyle: 'solid',
+    height: 50,
+    borderRadius: 8,
+    // minWidth: 343,
+    color: '#212121',
+    paddingLeft: 16,
+    marginTop: 16,
+  },
+
+  title: {
+    color: '#212121',
+    fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: '0.01em',
+    marginBottom: 16,
+    // justifyContent: 'center',
+    textAlign: 'center',
+  },
+
+  inputForm: {
+    marginHorizontal: 16,
+  },
+
+  btn: {
+    height: 51,
+    justifyContent: 'center',
+    borderRadius: 100,
+    marginTop: 43,
+    alignItems: 'center',
+    ...Platform.select({
+      ios: {
+        backgroundColor: '#FF6C00',
+      },
+      android: {
+        backgroundColor: '#FF6C00',
+      },
+    }),
+  },
+  btnText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  bottomtext: {
+    marginTop: 16,
+    textAlign: 'center',
+    color: '#1B4371',
+  },
+});
